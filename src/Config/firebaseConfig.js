80d5af9da20df6cc1c84dd5ecdb536131817fe0b
Amber Prakash Rai <ambarrai7@gmail.com>
2020-08-19 23:05:17 +0530

    initial commit

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyA59_ss6tP7GD9jDR6D2NOtuwB7pYDz0Xw",
+  authDomain: "mypracticepro-f7cbe.firebaseapp.com",
+  databaseURL: "https://mypracticepro-f7cbe.firebaseio.com",
+  projectId: "mypracticepro-f7cbe",
+  storageBucket: "mypracticepro-f7cbe.appspot.com",
+  messagingSenderId: "912888277891",
+  appId: "1:912888277891:web:f5958fdf1f0650fce6e4b6",
+  measurementId: "G-ZLCKN0PQJT"
+  };
---
+++ src/Layout/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Container} from "reactstrap"
+
+const Footer = () =>{
+    return(
+        <Container fluid tag="footer"
+        className="text-center bg-info text-white text-uppercase fixed-bottom p-3 ">
+            Amber Gtihub search app 
+        </Container>
+    )
+
+}
+
+export default Footer;
